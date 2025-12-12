24ed935faf23bc8a00ac0ff72333f88bddb5fa3c
lavanya9-tech <lavanyag48325@gmail.com>
2025-12-12 17:01:30 +0530

    Fixed merge conflict in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,35 +5,21 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-<<<<<<< HEAD
-  server: {
-    host: "::",
-=======
-  base: "/",  
+  base: "/",
 
   server: {
     host: true,
->>>>>>> 05f55b0c066731ec428969f1136cd63cf85e85d3
     port: 8080,
   },
+
   plugins: [
     react(),
-<<<<<<< HEAD
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-=======
     mode === "development" && componentTagger(),
   ].filter(Boolean),
 
->>>>>>> 05f55b0c066731ec428969f1136cd63cf85e85d3
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
-<<<<<<< HEAD
-=======
-
->>>>>>> 05f55b0c066731ec428969f1136cd63cf85e85d3
